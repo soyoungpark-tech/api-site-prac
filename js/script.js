bdf68a80a4e0264979d8d3fc5903c6e1345a355b
soyoungPark <ps.young149@gmail.com>
2021-06-26 11:25:33 +0900

    add code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
 'user strict';
 
-var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
+
+// 화면애니메이션
+const inner = document.querySelector(".inner");
+const section = document.querySelector("section");
+
+window.onscroll = function() {
+  let value = window.pageYOffset / section.offsetTop + 1;
+  inner.style.transform = `scale(${value})`;
 };
 
-var map = new naver.maps.Map('map', mapOptions);
+
+$(function(){
+	
+	var $window = $(window);		//Window object
+	
+	var scrollTime = 1.2;			//Scroll time
+	var scrollDistance = 170;		//Distance. Use smaller value for shorter scroll and greater value for longer scroll
+		
+	$window.on("mousewheel DOMMouseScroll", function(event){
+		
+		event.preventDefault();	
+										
+		var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+		var scrollTop = $window.scrollTop();
+		var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+			
+		TweenMax.to($window, scrollTime, {
+			scrollTo : { y: finalScroll, autoKill:true },
+				ease: Power1.easeOut,	//For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+				autoKill: true,
+				overwrite: 5							
+			});
+					
+	});
+	
+});
+
+// map
+// var mapOptions = {
+//     center: new naver.maps.LatLng(37.3595704, 127.105399),
+//     zoom: 10
+// };
+
+// var map = new naver.maps.Map('map', mapOptions);
